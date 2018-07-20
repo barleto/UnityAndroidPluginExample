589cc822c7cab494dc3ac9cceadf3450fe8fb608
Ricardo Charf <ricardo.charf@wavy.global>
2018-07-19 23:22:49 -0300

    initial commit

---
+++ Plugin/app/src/main/java/com/charf/plugin/Plugin.java
@@ -0,0 +1,9 @@
+package com.charf.plugin;
+
+import android.util.Log;
+
+public class Plugin {
+    public static void teste(String TAG, String message){
+        Log.d(TAG, message);
+    }
+}
---
+++ Plugin/lib/src/main/java/com/charf/lib/Plugin.java
@@ -0,0 +1,12 @@
+package com.charf.lib;
+
+import android.app.Activity;
+import android.util.Log;
+import com.unity3d.player.UnityPlayer;
+import com.unity3d.player.UnityPlayerActivity;
+
+public class Plugin {
+    public static void teste(String TAG, String message){
+        Log.e(TAG, message);
+    }
+}
